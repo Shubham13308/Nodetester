2a3bdb81b1282e64041ad3c326fd3774f17b75d5
Shubham Sharma <103982047+Shubham13308@users.noreply.github.com>
2025-01-06 16:53:12 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const fetchdata = require('./routes/userRoutes');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_DB_URI;
+
+app.use(cors());
+
+
+app.use('/group', fetchdata);
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch(err => {
+  console.error('MongoDB connection error:', err.message);
+  process.exit(1);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
