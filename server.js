f3e31dfc3824a9cd3d11ffad3b901e35240c1afd
Shubham Sharma <103982047+Shubham13308@users.noreply.github.com>
2025-01-06 17:02:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const MemberName = require('./models/MemberName');
+const fetchdata = require('./routes/userRoutes');
+const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT ;
+const port = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_DB_URI;
-const cors = require('cors');
+
 app.use(cors());
 
+
+app.use('/group', fetchdata);
+
 mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('MongoDB connection error:', err));
-
-
-app.get('/', async (req, res) => {
-    try {
-        const members = await MemberName.find(); 
-        console.log(members)
-        res.json(members); 
-    } catch (error) {
-        console.error('Error fetching members:', error);
-        res.status(500).json({ error: 'Failed to fetch members' });
-    }
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch(err => {
+  console.error('MongoDB connection error:', err.message);
+  process.exit(1);
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
